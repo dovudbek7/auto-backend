262277fdc4b257f30ed59c9cf9d4dc8abe80522f
dovudbek <111281179+dovudbek7@users.noreply.github.com>
2024-09-26 12:46:09 +0500

    add

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%i^_07e+_1#tu3po*b8m_&cw4ind96cla3jwa!pcct6-nx%#%l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*'
